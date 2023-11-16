921e4bfe8a4d311c6b8605e595dad89f9ee77f97
Anibal Rivero <riveroleonel@gmail.com>
2023-11-16 23:19:42 +1300

    Added Query to create table

--- conectionDB.js
+++ conectionDB.js
@@ -1,6 +1,17 @@
 import { createPool } from "mysql2/promise";
 import configuration from "./configuration.js";
 
+const CREATE_TABLE = `
+CREATE TABLE peliculas (
+    datos_peliculas INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL,
+    recaudacion_millones FLOAT NOT NULL DEFAULT 999999,
+    ganancias_millones FLOAT NOT NULL DEFAULT 999999,
+    secuela TINYINT NOT NULL DEFAULT false 
+);
+
+`;
+
 const pool = createPool({
     host: configuration.HOST,
     port: configuration.PORT,
@@ -9,6 +20,7 @@ const pool = createPool({
     database: configuration.NAME
 });
 
+ await pool.query(CREATE_TABLE);
 
 console.log("Database connection started");
 
